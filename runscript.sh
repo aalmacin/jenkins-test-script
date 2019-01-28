0a55f120c85ebd70b469a18b67f624f310c6693a
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-01-27 19:32:02 -0500

    Fixed logic error

--- runscript.sh
+++ runscript.sh
@@ -4,5 +4,5 @@ echo "What is your name?: $NAME"
 echo "What is the current year?: $YEAR"
 echo "What is your birth year?: $BYEAR"
 AGE=$(($YEAR - $BYEAR))
-echo "You are $AGE years old"
+echo "You will be $AGE years old at some point this year"
 
